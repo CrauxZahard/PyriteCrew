318a03f16d979eac8574640d84b2fd356f4faa5c
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-06-20 22:58:44 -0700

    Update notice.js
    bug fix

--- commands/notice.js
+++ commands/notice.js
@@ -2,10 +2,9 @@ module.exports = {
   name: 'notice',
   run: async (client, message, args) => {
     await message.delete();
-    const targetChannel = message.mentions.channels.first() == args[0] ? message.mentions.channels.first() : message.channel;
+    const targetChannel = client.getChannel(args[0]) ? message.mentions.channels.first() : message.channel;
     const webhook = await targetChannel.createWebhook(client.user.username, { avatar: client.user.displayAvatarURL() });
-    const content = message.mentions.channels.first() == args[0] ? args.slice(1).join(' ') : args.join(' ');
+    const content = client.getChannel(args[0]) ? args.slice(1).join(' ') : args.join(' ');
     webhook.send(content);
-    console.log(targetChannel)
   }
 }
