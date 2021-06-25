d57398f9e1b5fc23afd849e818593823ed64d945
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-06-24 19:28:02 -0700

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'userinfo',
+  run: async (client, message, args) => {
+      const target = args ? args[0] : message.author
+      const user = await client.users.fetch(target);
+      message.channel.send({description: `User: ${user.tag}\nCreation date: ${user.createdAt}`, color: 'RANDOM'}
+  }
+}
