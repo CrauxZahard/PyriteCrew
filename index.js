662e3d2c272edc36fb83959af8b7c899f46e0007
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-06 01:52:03 -0700

    Update index.js
    fixed something

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Database } = require('dbdjs.db');
 const fs = require('fs');
 const token = require('./configuration/token.json');
 
-require('./handler/database.js')(client, new Database); //load database handler
+require('./handler/database.js')(client, Database); //load database handler
 require('./handler/events.js')(client); //load event file handler
 require('./handler/file.js')(client); //load file handler
 
