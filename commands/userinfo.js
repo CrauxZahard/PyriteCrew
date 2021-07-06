ae1d7e435cfcdfcb5fd1ae6c787c15e09cc32c15
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-06 02:22:46 -0700

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,8 +1,7 @@
-module.exports = {
-  name: 'userinfo',
-  run: async (client, message, args) => {
+module.exports.name = 'userinfo';
+module.exports.aliases = ['ui', 'user', 'info'];
+module.exports.code = async (client, message, args) => {
       const target = args ? args[0] : message.author
       const user = await client.users.fetch(target);
-      message.channel.send('a', {embed: {description: `User: ${user.tag}\nCreation date: ${user.createdAt}`, color: 'RANDOM'}})
+      message.channel.send({embed: {description: `User: ${user.tag}\nCreation date: ${user.createdAt}`, color: 'RANDOM'}})
   }
-}
