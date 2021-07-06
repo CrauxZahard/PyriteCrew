0eab04275d0a1bef57168a06c04c5a9d7a5794b4
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-06 01:51:25 -0700

    Create database.js

---
+++ handler/database.js
@@ -0,0 +1,8 @@
+module.exports = (client, database) => {
+  const db = new database({
+  path: './databases/',
+  tables: [{name: 'cooldown'}, {name: 'main'}]
+  })
+  db.on('ready', () => console.log('database is ready'))
+  db.connect()
+}
