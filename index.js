d05e9f9472d2518c7e8c3c3a394ed919727b4487
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-06 01:45:20 -0700

    Update index.js
    made it more simple

--- index.js
+++ index.js
@@ -4,24 +4,11 @@ const { Database } = require('dbdjs.db');
 const fs = require('fs');
 const token = require('./configuration/token.json');
 
+require('./handler/database.js')(client, new Database); //load database handler
+require('./handler/events.js')(client); //load event file handler
+require('./handler/file.js')(client); //load file handler
+
 client.on('error', console.log)
-
-/* event handler */
-const events = fs.readdirSync('./events/')
-for (let event of events) {
-	let eventFile = require(`./events/${event}`)
-	client.on(event.split('.')[0], (...args) => eventFile(client, ...args))
-}
-
-/* database setup */
-const db = new Database({
-        path: './databases/',
-	tables: [{name: 'cooldown'}, {name: 'main'}]
-})
-
-db.on('ready', () => { console.log('database is ready') })
-db.connect()
-client.db = db
 
 /* logging in the bot to discord */
 client.login(token.token)
