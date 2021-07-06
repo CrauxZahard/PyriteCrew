c98f658e90a0ef366c0df24cf3321ec6eda2bc98
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-06 01:48:02 -0700

    Create events.js

---
+++ handler/events.js
@@ -0,0 +1,8 @@
+module.exports = client => {
+  const events = fs.readdirSync('./events/')
+for (let event of events) {
+	let eventFile = require(`./events/${event}`)
+	client.on(event.split('.')[0], (...args) => eventFile(client, ...args))
+}
+  
+}
