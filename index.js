e55864cfe95d1029775594cd1ed357a84a8fd140
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-06 04:02:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const token = require('./configuration/token.json');
 require('./handler/database.js')(client, Database); //load database handler
 require('./handler/events.js')(client); //load event file handler
 require('./handler/file.js')(client); //load file handler
+require('./handler/util.js')(client); //load util function
 
 client.on('error', console.log)
 
