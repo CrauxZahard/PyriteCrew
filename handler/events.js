d269831fa9c6ebece4b4f9861aeb67676e83f0d7
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-07 13:05:08 +0700

    Update events.js
    small fixes

--- handler/events.js
+++ handler/events.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 module.exports = client => {
-  const events = fs.readdirSync('./events/')
+  const events = fs.readdirSync('../events/')
 for (let event of events) {
-	let eventFile = require(`./events/${event}`)
+	let eventFile = require(`../events/${event}`)
 	client.on(event.split('.')[0], (...args) => eventFile(client, ...args))
 }
   
