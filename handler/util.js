8e480065e5e3268431a3ba8d34ccdac989061811
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-07 19:00:59 +0700

    Update util.js

--- handler/util.js
+++ handler/util.js
@@ -1,11 +1,13 @@
 module.exports = client => {
   client.getChannel = (channel, guild) => {
-    if (channel.startsWith('<#') && channel.endsWith('>')) {
+         if (!channel || !guild) return;
+         if (channel.startsWith('<#') && channel.endsWith('>')) {
       channel = channel.slice(2, -1)
       return guild.channels.cache.get(channel)
     }
   },
   client.getUser = user => {
+	  if (!user) return;
 	  if (user.startsWith('<@') && user.endsWith('>')) {
 		user = user.slice(2, -1);
 
