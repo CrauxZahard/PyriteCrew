91283e894443130560af64ea02f44bfa9935b098
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-07 18:27:48 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -6,26 +6,38 @@ module.exports = async (client, message) => {
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
     let commandName = args.shift().toLowerCase();
     let cmd = client.commands.get(commandName) || client.commands.get(client.aliases.get(commandName));
-    let cooldown = await client.db.get('cooldown', `${message.author.id}-${cmd.name}`)
     
+    /*if args[0] is a commmand name/alias, execute it*/
     if(cmd) {
+      let cooldown = await client.db.get('cooldown', `${message.author.id}-${cmd.name}`)
       
-      if (!cooldown) {
+      /*if user's cooldown is not in the database, set it*/
+      if(!cooldown) {
         await client.db.set('cooldown', `${message.author.id}-${cmd.name}`, Date.now())
-        }
-      
-      let time = Date.now();
-      cooldown = await client.db.get('cooldown', `${message.author.id}-${cmd.name}`)
-      if (time <= cooldown.value) {
-        let cooldownTime = cmd.cooldown * 1000 || 3 * 1000;
-        await cmd.code(client, message, args);
-        client.db.set('cooldown', `${message.author.id}-${cmd.name}`, time + cooldownTime)
-      }
-      else {
         cooldown = await client.db.get('cooldown', `${message.author.id}-${cmd.name}`)
-        let math = (cooldown.value - Date.now()) / 1000
+      }
+      
+      /*if cooldown time (in ms) is smaller than current date (in ms), execute the command*/
+      if(cooldown.value <= Date.now()) {
+        let cooldownAmount = cmd.cooldown * 1000 || 3 * 1000
+        try {
+          cmd.code(client, message, args)
+        }
+        catch (e) {
+          console.log(e)
+        }
+        finally {
+          await client.db.set('cooldown', `${message.author.id}-${cmd.name}`, Date.now() + cooldownAmount)
+        }
+      }
+      
+      /*else if user is on cooldown, give a message*/
+      else {
+        const math = cooldown.value - Date.now()
         message.reply(`please wait ${math.toFixed(1)} second(s) before using this command again.`)
       }
+      
     }
+    
   }
 }
