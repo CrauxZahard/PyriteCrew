7037fd7e716786d4a0ba08ed58cbd895a6b779ef
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-08 12:35:04 +0700

    added  1 more function

--- handler/util.js
+++ handler/util.js
@@ -17,5 +17,17 @@ module.exports = client => {
 
 		return client.users.cache.get(user);
 	 }
+  },
+  client.getMember = (member, guild) => {
+	  if (!member || !guild) return;
+	  if (member.startsWith('<@') && member.endsWith('>')) {
+		  member = member.slice(2, -1)
+		  
+		  if (member.startsWith('!') {
+		      member = member.slice(1)
+		      }
+		      
+		      return guild.members.cache.get(member);
+	  }
   }
 }
