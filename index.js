9f7ac5db949c5c288a8a8e2c84630e80007199ad
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-15 19:21:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ['REACTION', 'MESSAGE', 'CHANNEL'] });
-const { Database } = require('dbdjs.db');
 const { token } = require('./configuration/token.json');
 
-require('./handler/database.js')(client, Database);
+require('./handler/database.js')(client);
 require('./handler/events.js')(client); 
 require('./handler/file.js')(client);
 require('./handler/util.js')(client); 
