4d4402b71b291956f4bb8d9eb99fe350c6f9f340
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-15 19:20:09 +0700

    Update database.js

--- handler/database.js
+++ handler/database.js
@@ -1,9 +1,4 @@
-module.exports = (client, database) => {
-  const db = new database({
-  path: './databases/',
-  tables: [{name: 'cooldown'}, {name: 'main'}]
-  })
-  db.on('ready', () => console.log('database is ready'))
-  db.connect()
+const db = require('quick.db');
+module.exports = client => {
   client.db = db
 }
