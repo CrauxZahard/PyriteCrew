5308618d7531556d2664c8c776cdbb63181c3fc0
CrauxZahard <83466517+CrauxZahard@users.noreply.github.com>
2021-07-16 18:26:04 +0700

    Update notice.js

--- commands/1. Main Folder/notice.js
+++ commands/1. Main Folder/notice.js
@@ -3,8 +3,9 @@ module.exports.aliases = ['n', 'send']
 module.exports.cooldown = 5
 module.exports.code = async (client, message, args) => {
     await message.delete();
-    const targetChannel = client.getChannel(args[0], message.guild) ? message.mentions.channels.first() : message.channel;
+    const channel = await client.getChannel(args[0], message.guild)
+    const targetChannel = channel || message.channel ;
     const webhook = await targetChannel.createWebhook(client.user.username, { avatar: client.user.displayAvatarURL() });
-    const content = client.getChannel(args[0], message.guild) ? args.slice(1).join(' ') : args.join(' ');
+    const content = channel ? args.slice(1).join(' ') : args.join(' ');
     webhook.send(content);
   }
